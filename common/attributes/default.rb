f54ba4df42d3b78caac3895608b9895d306b48f8
Yasuhisa Yamazaki <1@yasu21.net>
2016-05-02 15:29:27 +0900

    Tune performance of nginx and gunicorn.

--- common/attributes/default.rb
+++ common/attributes/default.rb
@@ -4,3 +4,5 @@ default['app']['revision'] = 'master'
 default['nginx']['cookbook'] = 'nginx'
 default['nginx']['keepalive'] = 'on'
 default['nginx']['keepalive_timeout'] = 2
+default['gunicorn']['worker_max_requests'] = 8192
+default['gunicorn']['worker_timeout'] = 60
